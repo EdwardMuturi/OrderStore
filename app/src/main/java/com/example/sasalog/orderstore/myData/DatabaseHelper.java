4393163203a66e81916da2451a54aab9eea3ea8a
d.murimi14@gmail.com <d.murimi14@gmail.com>
2017-09-13 12:35:56 +0300

    All tables created

--- app/src/main/java/com/example/sasalog/orderstore/myData/DatabaseHelper.java
+++ app/src/main/java/com/example/sasalog/orderstore/myData/DatabaseHelper.java
@@ -20,22 +20,53 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         super (context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    //create tables
+    //customer table sql
+    private static String SQL_CREATE_CUSTOMER_TABLE="CREATE TABLE " + OrderStoreContract.OrderStoreEntry.TABLE_CUSTOMER + " (" +
+            OrderStoreContract.OrderStoreEntry._ID +" INTEGER PRIMARY KEY AUTOINCREMENT," +
+            OrderStoreContract.OrderStoreEntry.COLUMN_FIRST_NAME + "TEXT, " +
+            OrderStoreContract.OrderStoreEntry.COLUMN_LAST_NAME + "TEXT , " +
+            OrderStoreContract.OrderStoreEntry.COLUMN_TELEPHONE + "TEXT " +
+            "); ";
+    //product table sql
+    private static String SQL_CREATE_PRODUCT_TABLE="CREATE TABLE " + OrderStoreContract.OrderStoreEntry.TABLE_PRODUCT + " (" +
+            OrderStoreContract.OrderStoreEntry._ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            OrderStoreContract.OrderStoreEntry.COLUMN_PRODUCT_NAME + "TEXT , " +
+            OrderStoreContract.OrderStoreEntry.COLUMN_PRICE + "INTEGER" +
+            "); ";
+    //order table sql
+    private static String SQL_CREATE_ORDERS_TABLE= "CREATE TABLE " + OrderStoreContract.OrderStoreEntry.TABLE_ORDERS + " (" +
+            OrderStoreContract.OrderStoreEntry._ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
+           // OrderStoreContract.OrderStoreEntry.COLUMN_PRICE + "INTEGER, " +
+            OrderStoreContract.OrderStoreEntry.COLUMN_QUANTITY + "INTEGER, " +
+            OrderStoreContract.OrderStoreEntry.COLUMN_TOTAL_AMOUNT +"INTEGER" +
+            ") ";
+    private static String SQL_CREATE_ORDERLIST_TABLE= "CREATE TABLE" + OrderStoreContract.OrderStoreEntry.TABLE_ORDERLIST + " (" +
+            OrderStoreContract.OrderStoreEntry._ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            OrderStoreContract.OrderStoreEntry.COLUMN_CUSTOMER_ID + "INTEGER, " +
+            OrderStoreContract.OrderStoreEntry.COLUMN_PRODUCT_ID + "INTEGER, " +
+            OrderStoreContract.OrderStoreEntry.COLUMN_ORDER_ID + "INTEGER" +
+            ") ";
+
+
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase){
-        //create table
-        final String SQL_CREATE_CUSTOMER_TABLE="CREATE TABLE " + OrderStoreContract.OrderStoreEntry.TABLE_CUSTOMER + " (" +
-                OrderStoreContract.OrderStoreEntry._ID +" INTEGER PRIMARY KEY AUTOINCREMENT," +
-                OrderStoreContract.OrderStoreEntry.COLUMN_FIRST_NAME + "TEXT NOT NULL, " +
-                OrderStoreContract.OrderStoreEntry.COLUMN_LAST_NAME + "TEXT NOT NULL, " +
-                OrderStoreContract.OrderStoreEntry.COLUMN_TELEPHONE + "TEXT NOT NULL" +
-                "); ";
-        //executing above created query
+        //execute sql statements
         sqLiteDatabase.execSQL(SQL_CREATE_CUSTOMER_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_PRODUCT_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_ORDERS_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_ORDERLIST_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        //delete old tables upon data update
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + OrderStoreContract.OrderStoreEntry.TABLE_CUSTOMER);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + OrderStoreContract.OrderStoreEntry.TABLE_PRODUCT);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + OrderStoreContract.OrderStoreEntry.TABLE_ORDERS);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + OrderStoreContract.OrderStoreEntry.TABLE_ORDERLIST);
+
+        //create new table
         onCreate(sqLiteDatabase);
     }
 }
