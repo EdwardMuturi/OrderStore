92dd0cd2559b870693175abcff147f9aa0ccb457
d.murimi14@gmail.com <d.murimi14@gmail.com>
2017-09-14 14:58:03 +0300

    Completed recycler view for customer data

--- app/src/main/java/com/example/sasalog/orderstore/MainActivity.java
+++ app/src/main/java/com/example/sasalog/orderstore/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.sasalog.orderstore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int NUM_LIST_ITEMS= 100;
+
+    private CustomerAdapter mAdapter;
+    private RecyclerView customerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        customerList= (RecyclerView) findViewById(R.id.customer_recycler);
+
+        LinearLayoutManager layoutManager= new LinearLayoutManager(this);
+
+        customerList.setLayoutManager(layoutManager);
+        customerList.setHasFixedSize(true);
+        
+        mAdapter= new CustomerAdapter(NUM_LIST_ITEMS);
+
+        customerList.setAdapter(mAdapter);
     }
 }
