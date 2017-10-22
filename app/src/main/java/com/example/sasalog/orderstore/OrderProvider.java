04cc9592108e4e61b55e18ec056bf1a0a686603c
EdwardMuturi <kakaturi300@gmail.com>
2017-10-22 22:16:15 +0300

    Query method working on multiple tables; products and customers

--- app/src/main/java/com/example/sasalog/orderstore/OrderProvider.java
+++ app/src/main/java/com/example/sasalog/orderstore/OrderProvider.java
@@ -5,10 +5,12 @@ import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.provider.BaseColumns;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -19,6 +21,8 @@ import static com.example.sasalog.orderstore.myData.OrderProviderContract.AUTHOR
 import static com.example.sasalog.orderstore.myData.OrderProviderContract.CUSTOMER_BASE_PATH;
 import static com.example.sasalog.orderstore.myData.OrderProviderContract.CUSTOMER;
 import static com.example.sasalog.orderstore.myData.OrderProviderContract.CUSTOMER_ID;
+import static com.example.sasalog.orderstore.myData.OrderProviderContract.ORDER;
+import static com.example.sasalog.orderstore.myData.OrderProviderContract.ORDER_ID;
 import static com.example.sasalog.orderstore.myData.OrderProviderContract.PRODUCT;
 import static com.example.sasalog.orderstore.myData.OrderProviderContract.PRODUCT_BASE_PATH;
 import static com.example.sasalog.orderstore.myData.OrderProviderContract.PRODUCT_ID;
@@ -63,34 +67,41 @@ public class OrderProvider extends ContentProvider {
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[]
             selectionArgs, @Nullable String sortOrder) {
-        String tableName= OrderStoreContract.OrderStoreEntry.TABLE_CUSTOMER;
-        String[] columnRange= OrderStoreContract.OrderStoreEntry.ALL_CUSTOMERS;
         Cursor myCursor = null;
+        SQLiteQueryBuilder builder= new SQLiteQueryBuilder();
         int match =uriMatcher.match(uri);
 
         switch (match){
             case CUSTOMER:
-//                myCursor= database.query(OrderStoreContract.OrderStoreEntry.TABLE_CUSTOMER, OrderStoreContract.OrderStoreEntry.ALL_CUSTOMERS,
-//                        selection, null, null, null,
-//                        BaseColumns._ID + " DESC");
-                  tableName= OrderStoreContract.OrderStoreEntry.TABLE_CUSTOMER;
-                columnRange= OrderStoreContract.OrderStoreEntry.ALL_CUSTOMERS;
+                builder.setTables(OrderStoreContract.OrderStoreEntry.TABLE_CUSTOMER);
                 break;
+
             case CUSTOMER_ID:
-                selection = BaseColumns._ID + "=" + uri.getLastPathSegment();
+                builder.setTables(OrderStoreContract.OrderStoreEntry.TABLE_CUSTOMER);
+                builder.appendWhere(BaseColumns._ID + "= " + uri.getLastPathSegment());
                 break;
+
             case PRODUCT:
-//                myCursor= database.query(OrderStoreContract.OrderStoreEntry.TABLE_PRODUCT, OrderStoreContract.OrderStoreEntry.ALL_PRODUCTS, selection, null, null, null,
-//                        BaseColumns._ID + " ASC");
-                tableName= OrderStoreContract.OrderStoreEntry.TABLE_PRODUCT;
-                columnRange= OrderStoreContract.OrderStoreEntry.ALL_PRODUCTS;
+                builder.setTables(OrderStoreContract.OrderStoreEntry.TABLE_PRODUCT);
+                break;
+            case PRODUCT_ID:
+                builder.setTables(OrderStoreContract.OrderStoreEntry.TABLE_PRODUCT);
+                builder.appendWhere(BaseColumns._ID + "= " + uri.getLastPathSegment());
+                break;
+            case ORDER:
+                builder.setTables(OrderStoreContract.OrderStoreEntry.TABLE_ORDERS);
+                break;
+            case ORDER_ID:
+                builder.setTables(OrderStoreContract.OrderStoreEntry.TABLE_ORDERS);
+                builder.appendWhere(BaseColumns._ID + "= " +uri.getLastPathSegment());
                 break;
 
             default:
-                return myCursor;
+               throw new IllegalArgumentException("Unsupported URI: " + uri);
         }
-        return database.query(tableName, columnRange, selection, null, null, null,
-                BaseColumns._ID + " ASC");
+
+     Cursor cursor= builder.query(database, projection, selection, selectionArgs, null, null, null, sortOrder);
+        return cursor;
     }
 
 
